77176ed2c5a221fbc339a63d8797bdf8aad9e663
Sébastien Chèvre <seb.chevre@gmail.com>
2019-03-09 21:35:57 +0100

    correct sonar params 2

--- build/jelastic-stage-deploy.sh
+++ build/jelastic-stage-deploy.sh
@@ -43,6 +43,7 @@ deploy_stage() {
         # appel de l'api pour lancer le build et le deploy du projet
         DEPLOY_RESPONSE=$(curl "https://$JELASTIC_URL/1.0/environment/deployment/rest/builddeployproject?delay=1&envName=$ENV_NAME&session=$SESSION&nodeid=$NODE_ID&projectid=$PROJECT_ID&isSequential=false")
         
+        
 		echo $DEPLOY_RESPONSE
 		
 		if [DEPLOY_RESPONSE -eq 0]
